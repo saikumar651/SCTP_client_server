bce48ab2d62d31f9747162121d1a962310ca7dc8
5gtb <5gtb@gmail.com>
2019-08-27 12:09:27 +0530

    nothing important

--- sctp_client.c
+++ sctp_client.c
@@ -1,11 +1,11 @@
 #include <sys/socket.h>
-#include <sys/stat.h>
+#include <sys/types.h>
 #include <netinet/in.h>
+#include <netinet/sctp.h>
 #include <arpa/inet.h>
 
-#include <sys/types.h>
-#include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <errno.h>
@@ -23,17 +23,17 @@ void main()
 void tcp_client(int client_fd){
 
     struct sockaddr_in server_addr;
-    int ret;
- 
-    client_fd = socket(PF_INET, SOCK_STREAM, 0);
+
+    client_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_SCTP);
     if (client_fd == -1)
     	error(1, errno, "error opening socket");
 
+    bzero((void *)&server_addr,sizeof(server_addr));
    	server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    ret = connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    int ret = connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     if (ret == -1)
     	error(1, errno, "error connecting to server,check whether the server is running");
     close(client_fd);
--- sctp_server.c
+++ sctp_server.c
@@ -1,11 +1,11 @@
 #include <sys/socket.h>
-#include <sys/stat.h>
+#include <sys/types.h>
 #include <netinet/in.h>
+#include <netinet/sctp.h>
 #include <arpa/inet.h>
 
-#include <sys/types.h>
-#include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <errno.h>
@@ -24,27 +24,30 @@ void tcp_server(int client_fd,int server_fd){
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
 
-    server_fd = socket(PF_INET, SOCK_STREAM, 0);
+    server_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_SCTP);
     if (server_fd == -1)
     	error(1, errno, "error creating socket");
 
+    bzero((void *)&server_addr,sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     int ret = bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if (ret == -1)
-    	error(1, errno, "error binding to port 8085");
+    if (ret == -1){
+        close(server_fd);
+    	error(1, errno, "error binding to port\n");
+    }
 
-    int lis = listen(server_fd, 10);
+    int lis = listen(server_fd, 5);
     if (lis == -1)
-        error(1, errno, "error listening on socket");
+        error(1, errno, "error listening on socket\n");
          
     int len=sizeof(client_addr);
     client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &len);
     if (client_fd == -1)
-        error(1, errno,"error accepting connections");
+        error(1, errno,"error accepting connections\n");
     else
         printf("connection established succesfully\n");
-    close(server_fd);            
+    close(client_fd);            
 }
